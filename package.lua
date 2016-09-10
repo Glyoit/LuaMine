8fa04b0afe2c33b24e7ad507c0d9172fbc97fed5
Dog2puppy <colecooper2005@icloud.com>
2016-09-10 16:57:56 -0400

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "LuaMine",
+  version = "1.0.0",
+  license = "MPL-2.0",
+  homepage = "http://LuaMine.ml",
+  description = "Run a MCPE server in Lua using Luvit!",
+  tags = { "LuaMine", "MCPE" },
+  author = { name = "Dog2puppy" }
+}
