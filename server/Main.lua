379bb1544a5805c31479d3e69523df602f4b667d
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2016-09-10 22:40:24 -0400

    getting there

--- server/Main.lua
+++ server/Main.lua
@@ -1,4 +1,16 @@
-print('LuaMine is starting up...')
-print('Please wait...')
+--  _                 __  __ _            
+-- | |               |  \/  (_)           
+-- | |    _   _  __ _| \  / |_ _ __   ___ 
+-- | |   | | | |/ _` | |\/| | | '_ \ / _ \
+-- | |___| |_| | (_| | |  | | | | | |  __/
+-- |______\__,_|\__,_|_|  |_|_|_| |_|\___|
+--                                        
+-- @author LuaMine Team
+-- @website LuaMine.ml
+
+print('-------------------')
+print('LuaMine is starting.........')
+print('-------------------')
+
 
 local http = require('http')
