9d7cf4e273163594d8606284de95ad320c1f1c75
Dog2puppy <colecooper2005@icloud.com>
2016-09-19 06:48:39 -0400

    Update package.lua

--- package.lua
+++ package.lua
@@ -4,6 +4,13 @@ return {
   license = "MPL-2.0",
   homepage = "http://LuaMine.ml",
   description = "Run a MCPE server in Lua using Luvit!",
+  dependencies = {
+    "luvit/require",
+    "luvit/pretty-print",
+    "luvit/http-codec",
+    "luvit/json",
+    "luvit/http"
+  }
   tags = { "LuaMine", "MCPE" },
   author = { name = "LuaMine" }
 }
